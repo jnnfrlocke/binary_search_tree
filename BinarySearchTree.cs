c5ca8e40bbc48d5a17894f4f5a0cf113d3ead982
Jen <jnnfrlocke@gmail.com>
2017-09-27 10:55:11 -0500

    create NewTree method

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Binary_Search_Tree
 {
-    class BinarySearchTree
+    public class BinarySearchTree
     {
+        private Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void NewTree()
+        {
+            root = new Node(15);
+            root.leftNode = new Node(7);
+            root.rightNode = new Node(20);
+            root.leftNode.leftNode = new Node(3);
+            root.leftNode.leftNode = new Node(10);
+            root.rightNode.leftNode = new Node(17);
+            root.rightNode.rightNode = new Node(26);
+        }
+
     }
 }
